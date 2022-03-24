012d72ff8e38a9c6d8c61b13a3025692cf601334
samuelthaiyil <sathaiyi@lakeheadu.ca>
2022-03-24 12:00:00 -0400

    Implemented account creation functionality with firebase auth

---
+++ app/src/main/java/com/example/freelanceapp/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.freelanceapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+
+}
--- app/src/main/java/com/example/freelanceapp/MainActivity.kt
+++ app/src/main/java/com/example/freelanceapp/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.freelanceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val createButton = findViewById<Button>(R.id.create_btn)
+        val fullName = findViewById<TextView>(R.id.full_name_text)
+        val email = findViewById<TextView>(R.id.email_text)
+        val password = findViewById<TextView>(R.id.password_text)
+        val confirmPassword = findViewById<TextView>(R.id.confirm_password_text)
+
+        createButton.setOnClickListener {
+
+              if (fullName.text.toString().isNotEmpty() &&
+                 email.text.toString().isNotEmpty() &&
+                 password.text.toString().isNotEmpty() &&
+                 confirmPassword.text.toString().isNotEmpty()
+             ) {
+                 if (confirmPassword.text.toString() == password.text.toString()) {
+                     auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                         .addOnCompleteListener(this) { task ->
+
+                         }
+                 } else {
+                     Toast.makeText(baseContext, "Password do not match. ",
+                         Toast.LENGTH_SHORT).show()
+                 }
+            } else {
+                  Toast.makeText(baseContext, "Not all fields have been entered. ",
+                      Toast.LENGTH_SHORT).show()
+              }
+        }
     }
+
+
 }
